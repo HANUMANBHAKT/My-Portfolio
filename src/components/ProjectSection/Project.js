9e4f9c264c3dc8e738853ae380dcef894410c462
HANUMANBHAKT <karanraj7462@gmail.com>
2025-08-10 08:55:18 +0530

    first commit

---
+++ src/components/ProjectSection/Project.js
@@ -0,0 +1,84 @@
+// Importing images
+import Prjm0 from "../../assets/internshify3.png";
+import prjm1 from "../../assets/internshify1.png";
+import prjm2 from "../../assets/internshify2.png";
+import prjm3 from "../../assets/internshify4.png";
+import ms1 from "../../assets/music layer2.png";
+import ms2 from "../../assets/musicplayer1.png";
+import ms3 from "../../assets/musicplayer1.png";
+import fbclone from "../../assets/fbclone.png";
+
+import itask from "../../assets/itask.png";
+import pick1 from "../../assets/library2.png";
+import pick2 from "../../assets/librsry1.png";
+import quiz1 from "../../assets/quiz1.png";
+import quiz2 from "../../assets/quiz2.png";
+import quiz3 from "../../assets/quiz3.png";
+import stop1 from "../../assets/stop1.png";
+import stop2 from "../../assets/stop2.png";
+import tictac1 from "../../assets/tictac1.png";
+import tictac2 from "../../assets/tictac2.png";
+import tictac3 from "../../assets/tictac3.png";
+import gym1 from "../../assets/resume1.png";
+import gym2 from "../../assets/resume2.png";
+import gym3 from "../../assets/resume3.png";
+import gym4 from "../../assets/reume4.png";
+
+import vibe1 from "../../assets/vibe1.png";
+import vibe2 from "../../assets/vibe2.png";
+import vibe3 from "../../assets/vibe3.png";
+import weather from "../../assets/weather.png";
+import todo from "../../assets/todo.png";
+import fake2 from "../../assets/fake2.png";
+import fake3 from "../../assets/fake3.png";
+import fake4 from "../../assets/fake4.png";
+import att from "../../assets/att.png";
+
+// Project Data
+const projectData = [
+  {
+    name: "Internsify an Edtech platform",
+    images: [Prjm0, prjm1, prjm2, prjm3],
+    techStack: ["Mern Stack"],
+    description:
+      "A comprehensive app built with React and Tailwind CSS for consuming educational student.",
+    projectLink: "https://new-i-nternshify-rjkaran.vercel.app/",
+  },
+  {
+    name: "Internsify an Edtech platform",
+    images: [Prjm0, prjm1, prjm2, prjm3],
+    techStack: ["talwind"],
+    description:
+      "A comprehensive app built with React and Tailwind CSS for consuming educational student.",
+    projectLink: "https://new-i-nternshify-rjkaran.vercel.app/",
+  },
+  {
+    name: "Vibe With Us - a fully responsive music website",
+    images: [ms1, ms2, ms3],
+    techStack: ["HTML,css,js"],
+    description:
+      "A music website created with JavaScript that adapts seamlessly across devices.",
+    projectLink: "https://github.com/HANUMANBHAKT/college-library",
+  },
+  {
+    name: "College Library",
+    images: [pick1, pick2],
+    techStack: ["HTML"],
+    description:
+      "A React-based app that allows users to select and save books in library",
+    projectLink: "https://github.com/HANUMANBHAKT/college-library",
+  },
+
+  {
+    name: "Todo List App",
+    images: [todo],
+    techStack: ["Typescript"],
+    description:
+      "A simple and effective todo list application created with JavaScript for personal task management.",
+    projectLink: "https://github.com/HANUMANBHAKT/type-script-Add-todo",
+  },
+
+  // Add more projects as needed
+];
+
+export default projectData;
